e067f84dee16bd90bc315d6bd03895600fd07494
Doncamarillo <don_camarillo@yahoo.com>
2024-12-16 11:17:21 -0800

    Update requirements and app configuration

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.DEBUG)
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET')
 db = SQLAlchemy(app)
